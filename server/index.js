bb8fb3875c5e386cc28604f7d4331a89dfc9e591
SaadAyouj <saadayoujweb@gmail.com>
2020-12-11 02:38:40 +0100

    connected server to database mongo atlas

--- server/index.js
+++ server/index.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://memo:memo123@cluster0.qtlwz.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);    
